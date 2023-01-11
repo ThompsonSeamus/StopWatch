1eba192e2a7dde2552380efc3fe26cddb875e53e
seamu.th2363 <seamu.th2363@vusd.us>
2023-01-11 15:27:42 -0800

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timeText;
+    Button startButton, resetButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initialize
+        timeText = findViewById(R.id.timeText);
+        startButton = findViewById(R.id.startButton);
+        resetButton = findViewById(R.id.ResetButton);
+
+        //onClickListeners
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //update UI thread (I think)
+                startButton.setText(R.string.real_stop);
+
+                //Executive services
+                ExecutorService service = Executors.newSingleThreadExecutor();
+                service.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int milSecond = 100;
+                        int second = milSecond/1000;
+                        
+
+                    }
+                });
+
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+}
